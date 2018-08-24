e03f64388a952b2d21a5d69ee92df14aeaf1527d
Saikumar5679 <35543689+Saikumar5679@users.noreply.github.com>
2018-08-24 17:52:39 -0500

    First In class programming assignment

---
+++ ICP-1/Source/first_2_ICP1.py
@@ -0,0 +1,6 @@
+a=int(input("enter one number:"))
+b=int(input("Enter th other number:"))
+print("addition:",a+b)
+print("multiplication:",a*b)
+print("division:",a/b)
+print("exponent:",a**b)
---
+++ ICP-1/Source/first_ICP.py
@@ -0,0 +1,3 @@
+f_name=input("Enter the first name:")
+l_name=input("enter the last name:")
+print(f_name[::-2],l_name[::-2])
